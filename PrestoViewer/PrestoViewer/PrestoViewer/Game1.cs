7253e1df00507303a1f65409dd55bf9005b38e19
Adrian Mullings <adralemechk@gmail.com>
2013-10-20 01:34:40 -0400

    Framework for switching shading modes Keyboard zoom

--- PrestoViewer/PrestoViewer/PrestoViewer/Game1.cs
+++ PrestoViewer/PrestoViewer/PrestoViewer/Game1.cs
@@ -14,8 +14,7 @@ namespace PrestoViewer
     /// <summary>
     /// This is the main type for your game
     /// </summary>
-    public class Game1 : Microsoft.Xna.Framework.Game
-    {
+    public class Game1 : Microsoft.Xna.Framework.Game {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         SpriteFont font;
@@ -26,12 +25,17 @@ namespace PrestoViewer
         Vector2 hMapPos;
         Vector2 palettePos;
         Vector2 spritePos;
-        Vector2 spriteSpd = new Vector2(50.0f, 50.0f);
-        Effect effect;
-        float scale = 1.0f;
+        Effect[] effect = new Effect[2];
 
-        public Game1()
-        {
+        const float BASE_SCALE = 4.0f;
+        float scale = BASE_SCALE;
+        int keyboardZoom = 0;
+        KeyboardState prevKeyState = Keyboard.GetState();
+
+        enum ShadingVersion { Ver1, Ver2 };
+        ShadingVersion version = ShadingVersion.Ver1;
+
+        public Game1() {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
@@ -42,10 +46,8 @@ namespace PrestoViewer
         /// related content.  Calling base.Initialize will enumerate through any components
         /// and initialize them as well.
         /// </summary>
-        protected override void Initialize()
-        {
+        protected override void Initialize() {
             this.IsMouseVisible = true;
-
             base.Initialize();
         }
 
@@ -53,8 +55,7 @@ namespace PrestoViewer
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
         /// </summary>
-        protected override void LoadContent()
-        {
+        protected override void LoadContent() {
             int w = graphics.GraphicsDevice.Viewport.Width;
             int h = graphics.GraphicsDevice.Viewport.Height;
 
@@ -74,15 +75,15 @@ namespace PrestoViewer
 
             spritePos = new Vector2(w * 0.5f, h * 0.5f);
 
-            effect = Content.Load<Effect>("Effect1");
+            effect[0] = Content.Load<Effect>("Effect1");
+            //effect[1] = Content.Load<Effect>("Effect2");
         }
 
         /// <summary>
         /// UnloadContent will be called once per game and is the place to unload
         /// all content.
         /// </summary>
-        protected override void UnloadContent()
-        {
+        protected override void UnloadContent() {
             // TODO: Unload any non ContentManager content here
         }
 
@@ -91,14 +92,24 @@ namespace PrestoViewer
         /// checking for collisions, gathering input, and playing audio.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Update(GameTime gameTime)
-        {
+        protected override void Update(GameTime gameTime) {
             // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed) {
                 this.Exit();
+            }
+
+            KeyboardState currKeyState = Keyboard.GetState();
+            if (currKeyState.IsKeyDown(Keys.Up) && !prevKeyState.IsKeyDown(Keys.Up)) {
+                keyboardZoom--;
+            }
+            if (currKeyState.IsKeyDown(Keys.Down) && !prevKeyState.IsKeyDown(Keys.Down)) {
+                keyboardZoom++;
+            }
+            prevKeyState = currKeyState;
 
             int zoomValue = Mouse.GetState().ScrollWheelValue / -120;
-            scale = 1.0f / (float) Math.Pow(2.0, zoomValue);
+
+            scale = BASE_SCALE / (float) Math.Pow(2.0, zoomValue + keyboardZoom);
 
             base.Update(gameTime);
         }
@@ -107,8 +118,7 @@ namespace PrestoViewer
         /// This is called when the game should draw itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Draw(GameTime gameTime)
-        {
+        protected override void Draw(GameTime gameTime) {
             GraphicsDevice.Clear(Color.AntiqueWhite);
 
             spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone);
@@ -126,12 +136,19 @@ namespace PrestoViewer
             lightDir.Normalize();
             spriteBatch.DrawString(font, "Light: " + lightDir, Vector2.Zero, Color.Black);
 
-            effect.Parameters["hMap"].SetValue(hMapTexture);
-            effect.Parameters["palette"].SetValue(paletteTexture);
-            effect.Parameters["lightDir"].SetValue(lightDir);
-            effect.Parameters["texWidth"].SetValue(cMapTexture.Width);
-            effect.Parameters["texHeight"].SetValue(cMapTexture.Height);
-            effect.CurrentTechnique.Passes[0].Apply();
+            Effect currentEffect = effect[(int) version];
+            switch (version) {
+                case ShadingVersion.Ver1:
+                    break;
+                case ShadingVersion.Ver2:
+                    break;
+            }
+            currentEffect.Parameters["hMap"].SetValue(hMapTexture);
+            currentEffect.Parameters["palette"].SetValue(paletteTexture);
+            currentEffect.Parameters["lightDir"].SetValue(lightDir);
+            currentEffect.Parameters["texWidth"].SetValue(cMapTexture.Width);
+            currentEffect.Parameters["texHeight"].SetValue(cMapTexture.Height);
+            currentEffect.CurrentTechnique.Passes[0].Apply();
 
             Vector2 spriteOrigin = new Vector2(cMapTexture.Width / 2.0f, cMapTexture.Height / 2.0f);
             spriteBatch.Draw(cMapTexture, spritePos, null, Color.White, 0.0f, spriteOrigin, scale, SpriteEffects.None, 0.0f);
